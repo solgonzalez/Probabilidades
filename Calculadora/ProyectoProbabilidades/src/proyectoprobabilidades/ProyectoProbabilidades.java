32cd8a7418c7f1463d90f7ba872ed90aa9539b5b
Sol Gonzalez <gmarisol0603@gmail.com>
2016-11-12 15:25:04 -0600

    Calculadora de probabilidades.
    - Interfaz para calculadora de probabilidades binomiales.

---
+++ Calculadora/ProyectoProbabilidades/src/proyectoprobabilidades/ProyectoProbabilidades.java
@@ -0,0 +1,20 @@
+
+package proyectoprobabilidades;
+/**
+ *
+ * @author gmari
+ */
+public class ProyectoProbabilidades {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Binomial().setVisible(true);
+            }
+        });
+    }
+    
+}
